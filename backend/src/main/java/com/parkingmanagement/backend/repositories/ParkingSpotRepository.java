a4d89085b1b226af8db6b7c5588586a71afa402f
Efoe Max <efoemaxi17@gmail.com>
2026-05-19 08:24:30 +0100

    Created Remaining Interfaces For The Remaing Classes

---
+++ backend/src/main/java/com/parkingmanagement/backend/repositories/ParkingSpotRepository.java
@@ -0,0 +1,10 @@
+package com.parkingmanagement.backend.repositories;
+
+import com.parkingmanagement.backend.models.ParkingSpot;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ParkingSpotRepository extends JpaRepository<ParkingSpot, String>{
+    ParkingSpot findByEmail(String email);
+}
---
+++ backend/src/main/java/com/parkingmanagement/backend/repositories/ReservationRepository.java
@@ -0,0 +1,10 @@
+package com.parkingmanagement.backend.repositories;
+
+import com.parkingmanagement.backend.models.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReservationRepository extends JpaRepository<Reservation, String> {
+     Reservation findByEmail(String email);
+}
